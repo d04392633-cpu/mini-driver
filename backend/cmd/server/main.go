b089b6c4bab5aebb742b4760d8d00cf568e5a8bb
d04392633-cpu <d04392633@gmail.com>
2026-06-02 20:44:40 +0500

    style: format Go code with gofmt

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,36 +1,35 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"log"
+	"net/http"
 )
 
-type Response struct{
+type Response struct {
 	Message string `json:"message"`
 }
 
 func main() {
 	e := echo.New()
 
-  // Разрешаем запросы от React
-    e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-        AllowOrigins: []string{"http://localhost:5173"},
-        AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
-    }))
+	// Разрешаем запросы от React
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+	}))
 
 	//Запросы
 	e.GET("/api/hello", handler)
 
-
 	// Старт с провркой на ошибки
-	if err := e.Start(":8080"); err != nil{
-	log.Fatal(err)
+	if err := e.Start(":8080"); err != nil {
+		log.Fatal(err)
 	}
 }
 
-	//GET запрос путь:"/api/hello"
-	func handler(c echo.Context) error {
-		return c.JSON(http.StatusOK, Response{Message: "ок"})
-	}
+// GET запрос путь:"/api/hello"
+func handler(c echo.Context) error {
+	return c.JSON(http.StatusOK, Response{Message: "ок"})
+}
