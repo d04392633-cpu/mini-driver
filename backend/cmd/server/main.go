aa23c187e07d41d6199dd29cb7e88ae04fbb875a
d04392633-cpu <d04392633@gmail.com>
2026-06-02 20:14:32 +0500

    feat: add React fetch to Go API

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -14,19 +14,23 @@ type Response struct{
 func main() {
 	e := echo.New()
 
-	e.GET("/api/hello", handler)
-
   // Разрешаем запросы от React
     e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
         AllowOrigins: []string{"http://localhost:5173"},
         AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
     }))
 
+	//Запросы
+	e.GET("/api/hello", handler)
+
+
+	// Старт с провркой на ошибки
 	if err := e.Start(":8080"); err != nil{
 	log.Fatal(err)
 	}
 }
 
-func handler(c echo.Context) error {
-	return c.JSON(http.StatusOK, Response{Message: "ок"})
-}
+	//GET запрос путь:"/api/hello"
+	func handler(c echo.Context) error {
+		return c.JSON(http.StatusOK, Response{Message: "ок"})
+	}
